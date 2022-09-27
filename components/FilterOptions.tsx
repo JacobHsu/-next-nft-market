eda97aa7a9b05eac4b28ec0a667901643e836c02
jacobhsu <jacob.hsu.tw@gmail.com>
2022-09-27 11:12:27 +0800

    feat: Marketplace NextPage

---
+++ components/FilterOptions.tsx
@@ -0,0 +1,200 @@
+import React, { FC, useEffect, useMemo, useState } from "react";
+import {
+  Typography,
+  Box,
+  Divider,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  Slider,
+  Paper
+} from "@mui/material";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+
+const AccordionSx = {
+  p: "0 0 6px 0",
+  minHeight: 0,
+  "& .Mui-expanded": {
+    m: "0px",
+    minHeight: 0,
+  },
+  "& .MuiAccordionSummary-content": {
+    m: "0 0 0 6px",
+  },
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+};
+
+interface IAccordionProps {
+  title: string;
+  children: React.ReactElement;
+  noDivider?: boolean;
+}
+
+const FilterAccordionItem: FC<IAccordionProps> = ({
+  title,
+  children,
+  noDivider,
+}) => {
+  return (
+    <>
+      <Accordion
+        sx={{
+          background: "transparent",
+          boxShadow: "none",
+          "&:before": {
+            background: "transparent",
+          },
+        }}
+        disableGutters
+      >
+        <AccordionSummary
+          expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "14px" }} />}
+          aria-controls={`panel-${title.replace(/\s/g, "")}`}
+          id={`panel-${title.replace(/\s/g, "")}-header`}
+          sx={AccordionSx}
+        >
+          <Typography>{title}</Typography>
+        </AccordionSummary>
+        <AccordionDetails sx={{ p: 0 }}>
+          <FormGroup
+            sx={{
+              "& .MuiFormControlLabel-root .MuiTypography-root": {
+                fontSize: "16px",
+              },
+            }}
+          >
+            {children}
+          </FormGroup>
+        </AccordionDetails>
+      </Accordion>
+      {!noDivider && (
+        <Divider sx={{ background: "rgba(255,255,255,0.005)", my: "12px" }} />
+      )}
+    </>
+  );
+};
+
+const collectionOptions = [
+  "Ergopad Cubes",
+  "Wrath of Gods",
+  "Hello",
+  "Is it me",
+  "You're looking for?"
+]
+
+const FilterOptions = () => {
+  const priceRange = [0, 500];
+  const [ergPriceRange, setErgPriceRange] = useState<number[]>(priceRange);
+  const [collectionChecks, setCollectionChecks] = useState<boolean[]>([])
+  const [collectionSelectAll, setCollectionSelectAll] = useState<boolean>(true);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setErgPriceRange(newValue as number[]);
+  };
+
+  useMemo(() => {
+    const array = Array.from(collectionOptions, () => true);
+    setCollectionChecks(array)
+  }, [collectionOptions])
+
+  const collectionCheckHandleChange = (key: number) => {
+    setCollectionChecks(collectionChecks => collectionChecks.map((item, i) => i === key ? !item : item))
+  }
+
+  const collectionCheckSelectAll = (all: boolean) => {
+    setCollectionChecks(collectionChecks => collectionChecks.map(() => all ? true : false))
+    setCollectionSelectAll(!collectionSelectAll)
+  }
+
+  useEffect(() => {
+    const check = collectionChecks.every(Boolean)
+    if (check) {
+      setCollectionSelectAll(true)
+    }
+    if (!check) {
+      setCollectionSelectAll(false)
+    }
+  }, [collectionCheckHandleChange])
+
+  return (
+    <Paper
+      sx={{
+        p: "24px",
+      }}
+    >
+      <Typography variant="h6" sx={{ mb: '24px' }}>Filters</Typography>
+
+      {/* FILTER BY COLLECTION */}
+      <FilterAccordionItem title="Collections">
+        <Box sx={{ mx: "6px" }}>
+          {collectionOptions.map((option, i) => {
+            return (
+              <FormControlLabel
+                key={i}
+                checked={collectionChecks[i]}
+                onChange={() => collectionCheckHandleChange(i)}
+                control={<Checkbox sx={{ p: "6px 9px" }} size="small" />}
+                label={option}
+                sx={{ display: 'block' }}
+              />
+            )
+          })}
+          <FormControlLabel
+            checked={collectionSelectAll}
+            onChange={() => collectionCheckSelectAll(!collectionSelectAll)}
+            control={<Checkbox sx={{ p: "6px 9px" }} size="small" />}
+            label="Select All"
+          />
+        </Box>
+      </FilterAccordionItem>
+
+      {/* FILTER BY DAO SIZE */}
+      <FilterAccordionItem title="Price Range">
+        <Box sx={{ mx: "12px" }}>
+          <Slider
+            getAriaLabel={() => "Price Range"}
+            value={ergPriceRange}
+            onChange={handleChange}
+            valueLabelDisplay="auto"
+            min={priceRange[0]}
+            max={priceRange[1]}
+          />
+        </Box>
+      </FilterAccordionItem>
+
+      {/* FILTER BY DATE CREATED */}
+      <FilterAccordionItem title="Initiation Date">
+        <Box sx={{ mx: "6px" }}>
+          <FormControlLabel
+            control={<Checkbox sx={{ p: "6px 9px" }} size="small" />}
+            label="24 Hours"
+          />
+          <FormControlLabel
+            control={<Checkbox sx={{ p: "6px 9px" }} size="small" />}
+            label="Last week"
+          />
+          <FormControlLabel
+            control={<Checkbox sx={{ p: "6px 9px" }} size="small" />}
+            label="Past month"
+          />
+          <FormControlLabel
+            control={<Checkbox sx={{ p: "6px 9px" }} size="small" />}
+            label="Past year"
+          />
+          <FormControlLabel
+            control={<Checkbox sx={{ p: "6px 9px" }} size="small" />}
+            label="All time"
+          />
+        </Box>
+      </FilterAccordionItem>
+    </Paper>
+  );
+};
+
+export default FilterOptions;
---
+++ components/NftCard.tsx
@@ -0,0 +1,212 @@
+import React, { FC, useMemo } from 'react';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  CardActionArea,
+  Button,
+  Box,
+  Typography,
+  useTheme
+} from '@mui/material'
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Image from 'next/image';
+import Link from '@components/Link';
+import { ThemeContext } from '@emotion/react';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import { useRouter } from 'next/router'
+
+interface INftCardProps {
+  imgUrl?: string;
+  link?: string;
+  name?: string;
+  price?: string;
+  rarity?: string;
+  time?: string;
+  collection?: string;
+  collectionLink?: string;
+  artist?: string;
+  artistLogo?: string;
+  artistLink?: string;
+}
+
+const NftCard: FC<INftCardProps> = ({ imgUrl, link, name, price, rarity, time, collection, collectionLink, artist, artistLogo, artistLink }) => {
+  const router = useRouter();
+  
+  const randomInteger = (min: number, max: number) => {
+    return (min + Math.random() * (max - min)).toFixed();
+  };
+  const rand = useMemo(() => randomInteger(1, 18), [1, 18]);
+
+  const theme = useTheme()
+  return (
+    <Card
+      sx={{
+        minWidth: '156px',
+        mb: '6px',
+      }}
+    >
+      <CardActionArea
+        onClick={() => {
+          link && router.push(link);
+        }}
+      >
+        <Box sx={{ position: 'relative', display: 'block', height: { xs: '150px', sm: '205px' } }}>
+          <Image src={imgUrl ? imgUrl : `/images/placeholder/${rand}.jpg`} layout="fill" draggable="false" />
+        </Box>
+        {price && (
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '16px',
+              right: '16px',
+              p: '8px',
+              background: 'rgba(255,255,255,0.1)',
+              backdropFilter: 'blur(6px)',
+              color: '#fff',
+              borderRadius: '6px',
+            }}
+          >
+            <Typography sx={{ fontWeight: '700', }}>
+              {price}
+            </Typography>
+          </Box>
+        )}
+        <CardContent sx={{ position: 'relative' }}>
+          {/* {rarity && (
+            <Box
+              sx={{
+                position: 'absolute',
+                top: '-16px',
+                right: '16px',
+                height: '28px',
+                p: '2px 8px',
+                background: theme.palette.primary.main,
+                color: theme.palette.background.default,
+                borderRadius: '50px',
+              }}
+            >
+              <Typography sx={{ fontWeight: '700', }}>
+                {rarity}
+              </Typography>
+            </Box>
+          )} */}
+          <Typography
+            sx={{
+              fontWeight: '700',
+              fontSize: { xs: '0.9rem', sm: '1.1rem' },
+              mb: '6px',
+            }}
+          >
+            {name}
+          </Typography>
+          <Box>
+            <AccessTimeIcon
+              sx={{
+                verticalAlign: '-0.25em',
+                mr: '5px',
+              }}
+            />
+            <Typography
+              sx={{
+                display: 'inline-block',
+              }}
+            >
+              {time}
+            </Typography>
+          </Box>
+        </CardContent>
+      </CardActionArea>
+      <CardActions
+        sx={{
+          p: '16px',
+        }}
+      >
+        <Grid2
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{
+            width: '100%',
+            maxWidth: { xs: '130px', sm: 'none' }
+          }}
+        >
+          <Grid2>
+            <Typography
+              sx={{
+                fontWeight: '700',
+              }}
+            >
+              {collectionLink ? (
+                <Link
+                  href={collectionLink}
+                  sx={{
+                    color: theme.palette.text.secondary,
+                    textDecoration: 'none',
+                    '&:hover': {
+                      textDecoration: 'underline'
+                    },
+                  }}
+                >
+                  {collection}
+                </Link>
+              ) : (
+                collection
+              )}
+              {' '}
+              <Typography
+                component="span"
+                sx={{
+                  fontStyle: 'italic'
+                }}
+              >
+                by
+                {' '}
+                {artistLink ? (
+                  <Link
+                    href={artistLink}
+                    sx={{
+                      color: theme.palette.text.secondary,
+                      textDecoration: 'none',
+                      '&:hover': {
+                        textDecoration: 'underline'
+                      }
+                    }}
+                  >
+                    {artist}
+                  </Link>
+                ) : (
+                  artist
+                )}
+              </Typography>
+            </Typography>
+          </Grid2>
+
+          
+          <Grid2>
+            {artistLink && artistLogo ? (
+              <Link
+                href={artistLink}
+                sx={{
+                  color: theme.palette.text.secondary,
+                  textDecoration: 'none',
+                  '&:hover': {
+                    textDecoration: 'underline'
+                  },
+                }}
+              >
+                <Image src={artistLogo} layout="fixed" width={32} height={32} />
+              </Link>
+            ) : (
+              ''
+            )}
+          </Grid2>
+        </Grid2>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default NftCard;
